bc90cae57ee1d4d941d90156904e0dcaf8c9b1e4
Paul <webmoney91rus@yandex.ru>
2018-12-24 01:39:18 +0300

    swagger added

--- src/test/java/com/Paul/web/app/ApplicationTests.java
+++ src/test/java/com/Paul/web/app/ApplicationTests.java
@@ -1,10 +1,13 @@
 package com.Paul.web.app;
 
+import com.Paul.web.app.config.SwaggerConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Import(SwaggerConfig.class)
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
